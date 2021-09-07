c181c3a1708cde919a2e09aae6a7d1df1784e8eb
Kelig Villalard <colldark@outlook.com>
2021-09-07 08:21:08 +0200

    bis

---
+++ exercice_classe/Modeles/Maintenance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercice_classe.Modeles
+{
+    class Maintenance
+    {
+        #region attributs
+
+        #endregion
+
+        #region getter/setter
+
+        #endregion
+
+        #region constructeur
+
+        #endregion
+
+        #region methodes
+
+        #endregion
+    }
+}
