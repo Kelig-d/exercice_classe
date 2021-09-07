fae42ac37eac3a3e070f99a3d60cf77599ea4658
Kelig Villalard <colldark@outlook.com>
2021-09-07 08:20:51 +0200

    classe maintenance région

---
+++ exercice_classe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercice_classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
